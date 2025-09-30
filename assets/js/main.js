c5d63fa5586765d372d8c8dea145d4de68eb5394
Aitbead <129311877+Aitbead@users.noreply.github.com>
2025-09-30 17:37:28 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,5 @@
-function toggleMenu() {
-  const sidemenu = document.querySelector('.sidemenu');
-  sidemenu.classList.toggle('expanded');
-  sidemenu.classList.toggle('collapsed');
+function expand(x) {
+  x.classList.toggle("hamburger");
 }
+
 
