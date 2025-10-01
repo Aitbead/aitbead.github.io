979bbd23614b199d56c6170161a57925791797a2
Aitbead <129311877+Aitbead@users.noreply.github.com>
2025-10-01 18:04:29 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 function expand(x, y) {
   x.classList.toggle("hamburger");
-  y.forEach(label => label.classList.toggle('visible'));
+  y.forEach(label => label.classList.toggle('showlabel'));
 }
+
 
 
 
