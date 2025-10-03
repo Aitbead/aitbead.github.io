d669292f93b41c9a87bc158ccaed4af55ae6a5fc
Aitbead <129311877+Aitbead@users.noreply.github.com>
2025-10-03 22:35:18 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-function expand(x, y) {
+function expand(x) {
   x.classList.toggle("hamburger");
   document.querySelectorAll('.hidelabel, .showlabel').forEach(label => {
   label.classList.toggle('hidelabel');
