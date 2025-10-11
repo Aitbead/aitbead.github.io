98cb50155c86ad20f935be6cc4ce39179b237e6f
Aitbead <129311877+Aitbead@users.noreply.github.com>
2025-10-12 01:15:50 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,18 @@
 function expand(x) {
   x.classList.toggle("hamburger");
-  document.querySelectorAll('.hidelabel, .showlabel').forEach(label => {
-  label.classList.toggle('hidelabel');
-  label.classList.toggle('showlabel');
+  document.querySelectorAll(".hidelabel, .showlabel").forEach(label => {
+  label.classList.toggle("hidelabel");
+  label.classList.toggle("showlabel");
   });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".auto-link").forEach(link => {
+    let text = link.textContent.trim();
+    let slug = text
+        .toLowerCase()
+        .replace(/[\s\W]+/g, "-")
+        .replace(/^-+|-+$/g, "");
+    link.href = "/" + slug + "/index.html";
+  });
+});
