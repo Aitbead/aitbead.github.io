97d7db3d3066ef46777e4a3df9a08bebe6a298fe
Aitbead <129311877+Aitbead@users.noreply.github.com>
2025-10-12 20:20:18 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,11 @@ function expand(x) {
   });
 }
 
+const slugCounts = {};
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".autolink").forEach(link => {
-    let text = link.textContent.trim();
+    let text = link.textContent.trim().normalize("NFD");
+    text = text.replace(/[\u0300-\u036f]/g, '');
     let slug = text
         .toLowerCase()
         .replace(/[\s\W]+/g, "-")
@@ -16,4 +18,5 @@ document.addEventListener("DOMContentLoaded", () => {
     link.href = "/" + slug + "/index.html";
   });
 });
+
 
