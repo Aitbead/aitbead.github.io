384ebc55968af4846b9863f897b1a2ade5ffb4b8
Aitbead <129311877+Aitbead@users.noreply.github.com>
2025-10-12 20:32:21 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,17 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
     let text = link.textContent.trim().normalize("NFD");
     text = text.replace(/[\u0300-\u036f]/g, '');
     let slug = text
-      .toLowerCase()
-      .replace(/[\s\W]+/g, "-")
-      .replace(/^-+|-+$/g, "");
-    function uniqueSlug(text) {
-      const base = slugify(text);
-      const count = slugCounts[base] || 0;
-      slugCounts[base] = count + 1;
-    return count === 0 ? base : `${base}-${count + 1}`;
+        .toLowerCase()
+        .replace(/[\s\W]+/g, "-")
+        .replace(/^-+|-+$/g, "");
     link.href = "/" + slug + "/index.html";
   });
 });
-
-
-
