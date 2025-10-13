e67d6796836ef1d0ee286476cdba527329278b79
Aitbead <129311877+Aitbead@users.noreply.github.com>
2025-10-13 11:33:21 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,10 +12,12 @@ document.addEventListener("DOMContentLoaded", () => {
     let text = link.textContent.trim().normalize("NFD");
     text = text.replace(/[\u0300-\u036f]/g, '');
     let slug = text
-        .toLowerCase()
-        .replace(/[\s\W]+/g, "-")
-        .replace(/^-+|-+$/g, "");
+      .toLowerCase()
+      .replace(/[\s\W]+/g, "-")
+      .replace(/^-+|-+$/g, "")
+      .replace(/^home$/, '');
     link.href = "/" + slug;
   });
 });
+
 
